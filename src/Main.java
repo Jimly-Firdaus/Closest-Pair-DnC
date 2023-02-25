c262b383c342153ab001e3336ee9acb80a05f2a6
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-02-25 20:13:51 +0700

    feat: add time execution

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
 import type.ClosestPair;
-
+import java.time.Instant;
+import java.time.Duration;
 import java.util.Scanner;
 
 public class Main {
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.print("Total points: ");
         int size = scan.nextInt();
-        Points points = new Points(size, 100);
+        System.out.print("Dimension per point: ");
+        int dimension = scan.nextInt();
+        
+        Points points = new Points(size, dimension);
         Points points2 = points;
         ClosestPair ans, ans_;
-        ans = points.divideAndConquer(points.getPoints(), size, 0, 100);
+        Instant start = Instant.now();
+        ans = points.divideAndConquer(points.getPoints(), size, 0, dimension);
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("Divide & Conquer exec time: " + timeElapsed.toMillis() + " milliseconds");
         System.out.println("Divide & Conquer: " + ans.getDistance());
-        ans.getPair()[0].printVector();
-        ans.getPair()[1].printVector();
         System.out.println("Total count: " + points.getTotalDivideConquer());
-        System.out.println("\n=============================\n\n");
+        System.out.println("\n=============================\n");
+        Instant start_ = Instant.now();
         ans_ = points2.bruteForce(points2.getPoints());
+        Instant end_ = Instant.now();
+        Duration timeElapsed_ = Duration.between(start_, end_);
+        System.out.println("Brute Force exec time: " + timeElapsed_.toMillis() + " milliseconds");
         System.out.println("Brute Force: " + ans_.getDistance());
-        ans_.getPair()[0].printVector();
-        ans_.getPair()[1].printVector();
         System.out.println("Total count: " + points2.getTotalBruteForce());
     }
 
