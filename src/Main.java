c2bd6dcdc061b682e7169441da7465632ef133dc
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-02-26 01:26:32 +0700

    fix: update CLI

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,27 @@ import type.ClosestPair;
 import java.time.Instant;
 import java.time.Duration;
 import java.util.Scanner;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 
 public class Main {
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+        // system specs
+        OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+        String osName = osBean.getName();
+        String osArch = osBean.getArch();
+        String osVersion = osBean.getVersion();
+        int availableProcessors = osBean.getAvailableProcessors();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        System.out.println("Operating System: " + osName + " " + osVersion + " " + osArch);
+        System.out.println("Available Processors: " + availableProcessors);
+        System.out.println("Total Memory: " + totalMemory / 1024 / 1024 + " MB");
+
+        System.out.println("\n=============================\n");
         // prompt for input size & dimension
-        System.out.print("Total points: ");
+        System.out.print("Input total points: ");
         int size = scan.nextInt();
         System.out.print("Dimension per point: ");
         int dimension = scan.nextInt();
@@ -23,7 +37,7 @@ public class Main {
         Instant end = Instant.now();
         Duration timeElapsed = Duration.between(start, end);
 
-        System.out.println("\n\nDivide & Conquer exec time: " + timeElapsed.toSeconds() + " seconds");
+        System.out.println("\n\nDivide & Conquer execution time: " + timeElapsed.toSeconds() + " seconds");
         System.out.println("Divide & Conquer: " + ans.getDistance());
         System.out.println("Total count: " + Points.getTotalDivideConquer());
         System.out.println("\n=============================\n");
@@ -34,7 +48,7 @@ public class Main {
         Instant end_ = Instant.now();
         Duration timeElapsed_ = Duration.between(start_, end_);
 
-        System.out.println("Brute Force exec time: " + timeElapsed_.toSeconds() + " seconds");
+        System.out.println("Brute Force execution time: " + timeElapsed_.toSeconds() + " seconds");
         System.out.println("Brute Force: " + ans_.getDistance());
         System.out.println("Total count: " + Points.getTotalBruteForce());
     }
